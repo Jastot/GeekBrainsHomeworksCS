dbc231d0c22f28e9bfa62232f940bd611aa9eb6b
Jastot xD <george1005@yandex.ru>
2020-04-23 12:22:53 +0300

    FirstHW

---
+++ IMT/Program.cs
@@ -0,0 +1,31 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMT
+{
+    class Program
+    {
+        /*Ввести вес и рост человека.Рассчитать и вывести индекс массы тела(ИМТ) по формуле I=m/(h* h);
+        где m — масса тела в килограммах, h — рост в метрах.
+
+        Чудаков Г.А.
+        */
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите рост в формате а.аа : ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите вес: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(weight/Math.Pow(height,2));
+
+
+            Helpfull.Pause();
+        }
+    }
+}
---
+++ Library/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Helpfull
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+        public static void Pause(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
